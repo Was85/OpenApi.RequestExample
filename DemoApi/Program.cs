bfbf28787cfb565cad11839ab29fe5d6bbad0190
WA <waseem.alaabed@gmail.com>
2025-01-31 17:02:12 +0100

    clean test example and old files

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi(options =>
 {
-    options.AddDocumentTransformer<GeneratedOpenApiTransformers>();
+    options.AddDocumentTransformer<GeneratedOpenApiTransformer>();
 });
 
 var app = builder.Build();
@@ -32,36 +32,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-public class GeneratedOpenApiTransformers : IOpenApiDocumentTransformer
-{
-    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
-    {
-        // Adding example for /WeatherForecast (Get)
-        if (document.Paths.ContainsKey("/WeatherForecast"))
-        {
-            var operation = document.Paths["/WeatherForecast"].Operations[OperationType.Get];
-            if (operation.RequestBody?.Content?.ContainsKey("application/json") == true)
-            {
-                if (operation.RequestBody.Content["application/json"].Examples == null)
-                {
-                    operation.RequestBody.Content["application/json"].Examples = new Dictionary<string, OpenApiExample>();
-                }
-                operation.RequestBody.Content["application/json"].Examples["WeatherForecast Examples"] = WebApi_OpenApiExampleDemo.Controllers.WeatherForecastExamples.Example;
-            }
-        }
-        // Adding example for /WeatherForecast/test (Get)
-        if (document.Paths.ContainsKey("/WeatherForecast/test"))
-        {
-            var operation = document.Paths["/WeatherForecast/test"].Operations[OperationType.Get];
-            if (operation.RequestBody?.Content?.ContainsKey("application/json") == true)
-            {
-                if (operation.RequestBody.Content["application/json"].Examples == null)
-                {
-                    operation.RequestBody.Content["application/json"].Examples = new Dictionary<string, OpenApiExample>();
-                }
-                operation.RequestBody.Content["application/json"].Examples["WeatherForecast Examples"] = WebApi_OpenApiExampleDemo.Controllers.WeatherForecastExamples.Example;
-            }
-        }
-        return Task.CompletedTask;
-    }
-}
